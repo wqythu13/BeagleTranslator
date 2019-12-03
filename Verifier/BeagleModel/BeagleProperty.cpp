1d359415d66b463c3a6c4d44ea78cec08e3a0431
wangqinyu <wangqinyu303@gmail.com>
2019-12-03 15:41:55 +0800

    add beagleProperty

---
+++ Verifier/BeagleModel/BeagleProperty.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wqy on 19-12-3.
+//
+
+#include "BeagleProperty.h"
---
+++ Verifier/BeagleModel/BeagleProperty.h
@@ -0,0 +1,14 @@
+//
+// Created by wqy on 19-12-3.
+//
+
+#ifndef VERIFIER_BEAGLEPROPERTY_H
+#define VERIFIER_BEAGLEPROPERTY_H
+
+namespace esc {
+    class BeagleProperty {
+
+    };
+}
+
+#endif //VERIFIER_BEAGLEPROPERTY_H
