58c467d9a5c644c1153f86a53292050addd14493
wangqinyu <wangqinyu303@gmail.com>
2019-12-03 16:38:13 +0800

    add beagleVariable

---
+++ Verifier/BeagleModel/BeagleVariable.cpp
@@ -0,0 +1,16 @@
+//
+// Created by wqy on 19-12-3.
+//
+
+#include "BeagleVariable.h"
+using namespace esc;
+
+void BeagleVariable::setName(string _name)
+{
+    this->name = _name;
+}
+
+void BeagleVariable::setLen(int _len)
+{
+    this->len = _len;
+}
---
+++ Verifier/BeagleModel/BeagleVariable.h
@@ -0,0 +1,23 @@
+//
+// Created by wqy on 19-12-3.
+//
+
+#ifndef VERIFIER_BEAGLEVARIABLE_H
+#define VERIFIER_BEAGLEVARIABLE_H
+#include <string>
+using std::string;
+
+namespace esc {
+    class BeagleVariable {
+    private:
+        string name;
+        //Type type;
+        int len;
+
+    public:
+        void setName(string _name);
+        void setLen(int _len);
+    };
+}
+
+#endif //VERIFIER_BEAGLEVARIABLE_H
